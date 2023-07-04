5b59074732eae5b04a3907f93db4e44f5c2eaf22
simone-meneses <125327930+simone-meneses@users.noreply.github.com>
2023-07-04 19:27:41 -0300

    Adicionando parte 2 da tarefa

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
 window.onload = function() {
-    
     alert("A página acabou de ser carregada!");
+
+    var primeiraVariavel = 2;
+    var segundaVariavel = 3;
+
+    var resultadoElement = document.getElementById("resultado");
+    resultadoElement.textContent = "Primeira Variável: " + primeiraVariavel + "Segunda Variável: " + segundaVariavel;
+
+    var soma = primeiraVariavel + segundaVariavel;
+    var sub = primeiraVariavel - segundaVariavel;
+    var mult = primeiraVariavel * segundaVariavel;
+    var div = primeiraVariavel / segundaVariavel;
+
+    resultadoElement.innerHTML += "<br><br>Soma: " + soma + "<br>Subtração: " + sub + "<br>Multiplicação: " + mult + "<br>Divisão: " + div;
 
 };
